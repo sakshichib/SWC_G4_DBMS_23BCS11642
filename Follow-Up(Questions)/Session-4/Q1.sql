31259d1b323b600d99d29c1efb018b485ba67f28
Sakshi Chib <157032642+sakshichib@users.noreply.github.com>
2026-06-08 11:48:14 +0530

    Done

--- Follow-Up(Questions)/Session-4/Q1.sql
+++ Follow-Up(Questions)/Session-4/Q1.sql
@@ -1,1 +1,8 @@
-
+SELECT COUNT(*) AS payment_count
+FROM transactions t1
+JOIN transactions t2
+  ON t1.merchant_id = t2.merchant_id
+  AND t1.credit_card_id = t2.credit_card_id
+  AND t1.amount = t2.amount
+  AND t2.transaction_timestamp < t1.transaction_timestamp
+  AND t2.transaction_timestamp >= t1.transaction_timestamp - INTERVAL '10 minutes';
