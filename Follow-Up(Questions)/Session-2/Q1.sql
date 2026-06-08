351d8f6b23519c61be37108f8b129bf36db5e46e
Sakshi Chib <157032642+sakshichib@users.noreply.github.com>
2026-06-08 11:55:29 +0530

    Done

--- Follow-Up(Questions)/Session-2/Q1.sql
+++ Follow-Up(Questions)/Session-2/Q1.sql
@@ -1,1 +1,17 @@
 
+WITH first_purchase AS (
+    SELECT
+        user_id,
+        MIN(created_at) AS first_purchase_date
+    FROM amazon_transactions
+    GROUP BY user_id
+)
+
+SELECT DISTINCT
+    t.user_id
+FROM amazon_transactions t
+JOIN first_purchase fp
+    ON t.user_id = fp.user_id
+WHERE t.created_at > fp.first_purchase_date
+  AND DATE(t.created_at) - DATE(fp.first_purchase_date) BETWEEN 1 AND 7
+ORDER BY t.user_id;
