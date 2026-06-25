15358ae5dc892948892c67b3b5a9c777d2858005
Sakshi Chib <157032642+sakshichib@users.noreply.github.com>
2026-06-25 10:39:23 +0530

    Done

---
+++ Follow-Up(Questions)/Session-9/Q2.sql
@@ -0,0 +1,35 @@
+-- SELECT * FROM user_transactions;
+WITH yearly_spend AS (
+    SELECT
+        product_id,
+        EXTRACT(YEAR FROM transaction_date) AS year,
+        SUM(spend) AS curr_year_spend
+    FROM user_transactions
+    GROUP BY product_id,
+             EXTRACT(YEAR FROM transaction_date)
+),
+cte AS (
+    SELECT
+        product_id,
+        year,
+        curr_year_spend,
+        LAG(curr_year_spend)
+            OVER (
+                PARTITION BY product_id
+                ORDER BY year
+            ) AS prev_year_spend
+    FROM yearly_spend
+)
+SELECT
+    year,
+    product_id,
+    curr_year_spend,
+    prev_year_spend,
+    ROUND(
+        100.0 *
+        (curr_year_spend - prev_year_spend)
+        / prev_year_spend,
+        2
+    ) AS yoy_rate
+FROM cte
+ORDER BY product_id, year;
