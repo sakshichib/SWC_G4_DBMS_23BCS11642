54aaa5dc7a0cc41152c3bf11d18dc1b2868d3785
Sakshi Chib <157032642+sakshichib@users.noreply.github.com>
2026-06-25 10:25:15 +0530

    Done

---
+++ Follow-Up(Questions)/Session-9/Q1.sql
@@ -0,0 +1,10 @@
+-- SELECT * FROM reviews;
+
+WITH CTE AS (
+SELECT EXTRACT(MONTH FROM submit_date) AS mth, product_id AS product, ROUND(AVG(stars), 2) AS avg_stars
+FROM reviews
+GROUP BY mth, product_id
+ORDER BY mth, product
+)
+
+SELECT mth, product, avg_stars FROM CTE;
